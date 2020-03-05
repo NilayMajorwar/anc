552c6b77a0da1b20d7063b9a5cc6c3d9bdce8a47
parth <parthsrivastava100@gmail.com>
2020-03-06 00:15:58 +0530

    The updated contact details

--- src/js/main.js
+++ src/js/main.js
@@ -1,297 +1,276 @@
-$(document).ready(function() {
-  'use strict';
+ AOS.init({
+ 	duration: 800,
+ 	easing: 'slide',
+ 	once: false
+ });
 
-  var window_width = $(window).width(),
-    window_height = window.innerHeight,
-    header_height = $('.default-header').height(),
-    header_height_static = $('.site-header.static').outerHeight(),
-    fitscreen = window_height - header_height;
+jQuery(document).ready(function($) {
 
-  $('.fullscreen').css('height', window_height);
-  $('.fitscreen').css('height', fitscreen);
+	"use strict";
 
-  if (document.getElementById('default-select')) {
-    $('select').niceSelect();
-  }
+	$(".loader").delay(1000).fadeOut("slow");
+  $("#overlayer").delay(1000).fadeOut("slow");	
 
-  $('.img-pop-up').magnificPopup({
-    type: 'image',
-    gallery: {
-      enabled: true
-    }
-  });
+	var siteMenuClone = function() {
 
-  $('.play-btn').magnificPopup({
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false
-  });
+		$('.js-clone-nav').each(function() {
+			var $this = $(this);
+			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+		});
 
-  // Initiate superfish on nav menu
-  $('.nav-menu').superfish({
-    animation: {
-      opacity: 'show'
-    },
-    speed: 400
-  });
 
-  // Mobile Navigation
-  if ($('#nav-menu-container').length) {
-    var $mobile_nav = $('#nav-menu-container')
-      .clone()
-      .prop({
-        id: 'mobile-nav'
+		setTimeout(function() {
+			
+			var counter = 0;
+      $('.site-mobile-menu .has-children').each(function(){
+        var $this = $(this);
+        
+        $this.prepend('<span class="arrow-collapse collapsed">');
+
+        $this.find('.arrow-collapse').attr({
+          'data-toggle' : 'collapse',
+          'data-target' : '#collapseItem' + counter,
+        });
+
+        $this.find('> ul').attr({
+          'class' : 'collapse',
+          'id' : 'collapseItem' + counter,
+        });
+
+        counter++;
+
       });
-    $mobile_nav.find('> ul').attr({
-      class: '',
-      id: ''
-    });
-    $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="lnr lnr-menu"></i></button>');
-    $('body').append('<div id="mobile-body-overly"></div>');
-    $('#mobile-nav')
-      .find('.menu-has-children')
-      .prepend('<i class="lnr lnr-chevron-down"></i>');
 
-    $(document).on('click', '.menu-has-children i', function(e) {
-      $(this)
-        .next()
-        .toggleClass('menu-item-active');
-      $(this)
-        .nextAll('ul')
-        .eq(0)
-        .slideToggle();
-      $(this).toggleClass('lnr-chevron-up lnr-chevron-down');
+    }, 1000);
+
+		$('body').on('click', '.arrow-collapse', function(e) {
+      var $this = $(this);
+      if ( $this.closest('li').find('.collapse').hasClass('show') ) {
+        $this.removeClass('active');
+      } else {
+        $this.addClass('active');
+      }
+      e.preventDefault();  
+      
     });
 
-    $(document).on('click', '#mobile-nav-toggle', function(e) {
-      $('body').toggleClass('mobile-nav-active');
-      $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
-      $('#mobile-body-overly').toggle();
-    });
+		$(window).resize(function() {
+			var $this = $(this),
+				w = $this.width();
 
-    $(document).click(function(e) {
-      var container = $('#mobile-nav, #mobile-nav-toggle');
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
-          $('#mobile-body-overly').fadeOut();
-        }
+			if ( w > 768 ) {
+				if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+			}
+		})
+
+		$('body').on('click', '.js-menu-toggle', function(e) {
+			var $this = $(this);
+			e.preventDefault();
+
+			if ( $('body').hasClass('offcanvas-menu') ) {
+				$('body').removeClass('offcanvas-menu');
+				$this.removeClass('active');
+			} else {
+				$('body').addClass('offcanvas-menu');
+				$this.addClass('active');
+			}
+		}) 
+
+		// click outisde offcanvas
+		$(document).mouseup(function(e) {
+	    var container = $(".site-mobile-menu");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+	    }
+		});
+	}; 
+	siteMenuClone();
+
+
+	var sitePlusMinus = function() {
+		$('.js-btn-minus').on('click', function(e){
+			e.preventDefault();
+			if ( $(this).closest('.input-group').find('.form-control').val() != 0  ) {
+				$(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) - 1);
+			} else {
+				$(this).closest('.input-group').find('.form-control').val(parseInt(0));
+			}
+		});
+		$('.js-btn-plus').on('click', function(e){
+			e.preventDefault();
+			$(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) + 1);
+		});
+	};
+	// sitePlusMinus();
+
+
+	var siteSliderRange = function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
       }
     });
-  } else if ($('#mobile-nav, #mobile-nav-toggle').length) {
-    $('#mobile-nav, #mobile-nav-toggle').hide();
-  }
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+	};
+	// siteSliderRange();
 
-  // Smooth scroll for the menu and links with .scrollto classes
-  $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      if (target.length) {
-        var top_space = 0;
 
-        if ($('#header').length) {
-          top_space = $('#header').outerHeight();
+	
 
-          if (!$('#header').hasClass('header-fixed')) {
-            top_space = top_space;
-          }
-        }
+	var siteCarousel = function () {
+		if ( $('.nonloop-block-13').length > 0 ) {
+			$('.nonloop-block-13').owlCarousel({
+		    center: false,
+		    items: 1,
+		    loop: true,
+				stagePadding: 0,
+		    margin: 0,
+		    smartSpeed: 1000,
+		    autoplay: true,
+		    nav: true,
+				navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+		    responsive:{
+	        600:{
+	        	margin: 0,
+	        	nav: true,
+	          items: 2
+	        },
+	        1000:{
+	        	margin: 0,
+	        	stagePadding: 0,
+	        	nav: true,
+	          items: 2
+	        },
+	        1200:{
+	        	margin: 0,
+	        	stagePadding: 0,
+	        	nav: true,
+	          items: 3
+	        }
+		    }
+			});
+		}
 
-        $('html, body').animate(
-          {
-            scrollTop: target.offset().top - top_space
-          },
-          1500,
-          'easeInOutExpo'
-        );
+		$('.slide-one-item').owlCarousel({
+	    center: false,
+	    items: 1,
+	    loop: true,
+			stagePadding: 0,
+	    margin: 0,
+	    smartSpeed: 1500,
+	    autoplay: true,
+	    pauseOnHover: false,
+	    dots: true,
+	    nav: true,
+	    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+	  });
+	};
+	siteCarousel();
 
-        if ($(this).parents('.nav-menu').length) {
-          $('.nav-menu .menu-active').removeClass('menu-active');
-          $(this)
-            .closest('li')
-            .addClass('menu-active');
-        }
+	
 
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('lnr-times lnr-bars');
-          $('#mobile-body-overly').fadeOut();
-        }
-        return false;
-      }
-    }
-  });
+	var siteCountDown = function() {
 
-  $(document).ready(function() {
-    $('html, body').hide();
+		$('#date-countdown').countdown('2020/10/10', function(event) {
+		  var $this = $(this).html(event.strftime(''
+		    + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
+		    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
+		    + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
+		    + '<span class="countdown-block"><span class="label">%M</span> min </span>'
+		    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
+		});
+				
+	};
+	// siteCountDown();
 
-    if (window.location.hash) {
-      setTimeout(function() {
-        $('html, body')
-          .scrollTop(0)
-          .show();
+	var siteDatePicker = function() {
 
-        $('html, body').animate(
-          {
-            scrollTop: $(window.location.hash).offset().top - 62
-          },
-          1000
-        );
-      }, 0);
-    } else {
-      $('html, body').show();
-    }
-  });
+		if ( $('.datepicker').length > 0 ) {
+			$('.datepicker').datepicker();
+		}
 
-  // Header scroll class
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('#header').addClass('header-scrolled');
-    } else {
-      $('#header').removeClass('header-scrolled');
-    }
-  });
+	};
+	// siteDatePicker();
 
-  $('.active-video-carusel').owlCarousel({
-    items: 1,
-    loop: true,
-    dots: true,
-    margin: 30
-  });
+	var siteSticky = function() {
+		$(".js-sticky-header").sticky({topSpacing:0});
+	};
+	siteSticky();
 
-  $('.active-speaker-carusel').owlCarousel({
-    items: 2,
-    loop: true,
-    dots: true,
-    autoplay: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      1280: {
-        items: 2
-      }
-    }
-  });
+	// navigation
+  var OnePageNavigation = function() {
+    var navToggler = $('.site-menu-toggle');
 
-  //  Start Google map
+   	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a[href^='#']", function(e) {
+      e.preventDefault();
 
-  // When the window has finished loading create our google map below
+      var hash = this.hash;
 
-  if (document.getElementById('map')) {
-    google.maps.event.addDomListener(window, 'load', init);
+      $('html, body').animate({
+        'scrollTop': $(hash).offset().top - 50
+      }, 600, 'easeInOutExpo', function() {
+        // window.location.hash = hash;
 
-    function init() {
-      // Basic options for a simple Google Map
-      // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-      var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 11,
-
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.67, -73.94), // New York
-
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [
-          { featureType: 'water', elementType: 'geometry', stylers: [{ color: '#e9e9e9' }, { lightness: 17 }] },
-          { featureType: 'landscape', elementType: 'geometry', stylers: [{ color: '#f5f5f5' }, { lightness: 20 }] },
-          {
-            featureType: 'road.highway',
-            elementType: 'geometry.fill',
-            stylers: [{ color: '#ffffff' }, { lightness: 17 }]
-          },
-          {
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [{ color: '#ffffff' }, { lightness: 29 }, { weight: 0.2 }]
-          },
-          { featureType: 'road.arterial', elementType: 'geometry', stylers: [{ color: '#ffffff' }, { lightness: 18 }] },
-          { featureType: 'road.local', elementType: 'geometry', stylers: [{ color: '#ffffff' }, { lightness: 16 }] },
-          { featureType: 'poi', elementType: 'geometry', stylers: [{ color: '#f5f5f5' }, { lightness: 21 }] },
-          { featureType: 'poi.park', elementType: 'geometry', stylers: [{ color: '#dedede' }, { lightness: 21 }] },
-          {
-            elementType: 'labels.text.stroke',
-            stylers: [{ visibility: 'on' }, { color: '#ffffff' }, { lightness: 16 }]
-          },
-          { elementType: 'labels.text.fill', stylers: [{ saturation: 36 }, { color: '#333333' }, { lightness: 40 }] },
-          { elementType: 'labels.icon', stylers: [{ visibility: 'off' }] },
-          { featureType: 'transit', elementType: 'geometry', stylers: [{ color: '#f2f2f2' }, { lightness: 19 }] },
-          {
-            featureType: 'administrative',
-            elementType: 'geometry.fill',
-            stylers: [{ color: '#fefefe' }, { lightness: 20 }]
-          },
-          {
-            featureType: 'administrative',
-            elementType: 'geometry.stroke',
-            stylers: [{ color: '#fefefe' }, { lightness: 17 }, { weight: 1.2 }]
-          }
-        ]
-      };
-
-      // Get the HTML DOM element that will contain your map
-      // We are using a div with id="map" seen below in the <body>
-      var mapElement = document.getElementById('map');
-
-      // Create the Google Map using our element and options defined above
-      var map = new google.maps.Map(mapElement, mapOptions);
-
-      // Let's also add a marker while we're at it
-      var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(40.67, -73.94),
-        map: map,
-        title: 'Snazzy!'
       });
-    }
-  }
 
-  $(document).ready(function() {
-    $('#mc_embed_signup')
-      .find('form')
-      .ajaxChimp();
-  });
+    });
+  };
+  OnePageNavigation();
 
-  //   // Set the date we're counting down to
-  //   var countDownDate = new Date('Mar 30, 2019 12:00:00').getTime();
+  var siteScroll = function() {
 
-  //   // Update the count down every 1 second
-  //   var x = setInterval(function() {
-  //     // Get todays date and time
-  //     var now = new Date().getTime();
+  	
 
-  //     // Find the distance between now an the count down date
-  //     var distance = countDownDate - now;
+  	$(window).scroll(function() {
 
-  //     // Time calculations for days, hours, minutes and seconds
-  //     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  //     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  //     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  //     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  		var st = $(this).scrollTop();
 
-  //     // Output the result in an element with id="demo"
-  //     document.getElementById('timer').innerHTML =
-  //       "<div class='start-in'>starts in:</div>" +
-  //       days +
-  //       '<span>days  </span>: ' +
-  //       hours +
-  //       '<span>hours</span>: ' +
-  //       minutes +
-  //       '<span>mins  </span>: ' +
-  //       seconds +
-  //       '<span>secs  </span>';
+  		if (st > 100) {
+  			$('.js-sticky-header').addClass('shrink');
+  		} else {
+  			$('.js-sticky-header').removeClass('shrink');
+  		}
 
-  //     // If the count down is over, write some text
-  //     if (distance < 0) {
-  //       clearInterval(x);
-  //       document.getElementById('timer').innerHTML = 'ONGOING';
-  //     }
-  //   }, 1000);
+  	}) 
+
+  };
+  siteScroll();
+
+
+  var counter = function() {
+		
+		$('#about-section').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
+
+				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+				$('.number > span').each(function(){
+					var $this = $(this),
+						num = $this.data('number');
+					$this.animateNumber(
+					  {
+					    number: num,
+					    numberStep: comma_separator_number_step
+					  }, 7000
+					);
+				});
+				
+			}
+
+		} , { offset: '95%' } );
+
+	}
+	counter();
+
+
+
 });
