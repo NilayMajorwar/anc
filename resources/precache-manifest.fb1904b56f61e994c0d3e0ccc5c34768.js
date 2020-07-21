4426cdb1cb29e30f33601dd4e065f58129747248
Sanyog Gupta <sanyoggupta42@gmail.com>
2020-07-21 22:19:11 +0530

    Added auth on resources

---
+++ resources/precache-manifest.fb1904b56f61e994c0d3e0ccc5c34768.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222f9a1c460245a8964d66595657daf3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1e8112b59739d3d2db7c",
+    "url": "./static/css/2.d97600f8.chunk.css"
+  },
+  {
+    "revision": "661c9608d034a71d93c7",
+    "url": "./static/css/main.562395b0.chunk.css"
+  },
+  {
+    "revision": "1e8112b59739d3d2db7c",
+    "url": "./static/js/2.bcbbcd4c.chunk.js"
+  },
+  {
+    "revision": "545c8fe5f24124b468356ab857bb26cc",
+    "url": "./static/js/2.bcbbcd4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661c9608d034a71d93c7",
+    "url": "./static/js/main.4391ab3e.chunk.js"
+  },
+  {
+    "revision": "2b00759073bc710e4949",
+    "url": "./static/js/runtime-main.1ce6add8.js"
+  },
+  {
+    "revision": "10e22895eca106b6c38b8d2ef20f72c0",
+    "url": "./static/media/anclogo.10e22895.png"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "./static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "ffcc99c42164b65da7af6307b66379cf",
+    "url": "./static/media/header.ffcc99c4.jpg"
+  },
+  {
+    "revision": "265666a240b57b597e3658d1c537ab03",
+    "url": "./static/media/login.265666a2.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "./static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "./static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "./static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "./static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
